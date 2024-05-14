8b290aa39ebce4532655307ae5aa4651c6e15632
Generosa Litton <glcodeworks@gmail.com>
2024-05-14 15:24:41 -0500

    created json file for message prompts

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 const readline = require("readline-sync");
+const messages = require("./calculator_messages.json");
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -11,29 +12,27 @@ function invalidNumber(number) {
 prompt("Welcome to Calculator!");
 
 while (true) {
-  prompt("What's the first number? ");
+  prompt(messages.questions.firstNumber);
   let number1 = readline.question();
   while (invalidNumber(number1)) {
-    prompt(`Sorry you entered an invalid first number. Try again.`);
+    prompt(messages.questions.invalidNumber);
     number1 = readline.question();
   }
 
-  prompt("What's the second number? ");
+  prompt(messages.questions.secondNumber);
   let number2 = readline.question();
   while (invalidNumber(number2)) {
-    prompt(`Sorry you entered an invalid first number. Try again.`);
+    prompt(messages.questions.invalidNumber);
     number2 = readline.question();
   }
 
-  prompt(`You entered these numbers: ${number1} and ${number2}`);
+  prompt(`{messages.questions.confirmation} ${number1} and ${number2}`);
 
-  prompt(
-    "What operation would you like to perform? 1) Add 2) Subtract 3) Multiply 4) Divide"
-  );
+  prompt(messages.questions.operation);
   let operation = readline.question();
 
   while (!["1", "2", "3", "4"].includes(operation)) {
-    prompt("You must choose 1, 2, 3 or 4");
+    prompt(messages.questions.choice);
     operation = readline.question();
   }
 
@@ -62,35 +61,9 @@ while (true) {
     `The result of ${math} the numbers ${number1} and ${number2} is ${output} `
   );
 
-  prompt(
-    `Do you want to do another calculation? Type 'y' for yes or 'n' for no.`
-  );
+  prompt(messages.questions.anotherCalculation);
 
   let answer = readline.question();
 
   if (answer !== "y") break;
 }
-
-//Launch School version
-// const readline = require("readline-sync");
-
-// function prompt(message) {
-//   console.log(`=> ${message}`);
-// }
-
-// function invalidNumber(number) {
-//   return number.trimStart() === "" || Number.isNaN(Number(number));
-// }
-
-// prompt("Welcome to Calculator!");
-
-// while (true) {
-//   // ask for two numbers
-//   // ask for operation
-//   // perform operation and display results
-
-//   prompt("Would you like to perform another operation? (y/n)");
-//   let answer = readline.question();
-
-//   if (answer[0].toLowerCase() !== 'y') break;
-// }
