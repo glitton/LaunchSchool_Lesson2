ee2d4e2baf080550e2c2ffbb88fd1bda5d61c1b1
Generosa Litton <glcodeworks@gmail.com>
2024-05-14 15:05:29 -0500

    used while, created a clean calc file

--- calculator-1.js
+++ calculator-1.js
@@ -8,35 +8,37 @@ function invalidNumber(number) {
   return number.trimStart() === "" || Number.isNaN(Number(number));
 }
 
-prompt("Welcome to Calculator!");
-
-while (true) {
+function startCalculating() {
+  prompt("Welcome to Calculator!");
+  // Ask the user for the first number.
   prompt("What's the first number? ");
   let number1 = readline.question();
+
   while (invalidNumber(number1)) {
     prompt(`Sorry you entered an invalid first number. Try again.`);
     number1 = readline.question();
   }
-
+  // Ask the user for the second number.
   prompt("What's the second number? ");
   let number2 = readline.question();
   while (invalidNumber(number2)) {
-    prompt(`Sorry you entered an invalid first number. Try again.`);
+    prompt(`Sorry you entered an invalid second number. Try again.`);
     number2 = readline.question();
   }
 
   prompt(`You entered these numbers: ${number1} and ${number2}`);
 
+  // Ask the user for an operation to perform.
   prompt(
     "What operation would you like to perform? 1) Add 2) Subtract 3) Multiply 4) Divide"
   );
   let operation = readline.question();
 
   while (!["1", "2", "3", "4"].includes(operation)) {
-    prompt("You must choose 1, 2, 3 or 4");
+    prompt("Must choose 1, 2, 3 or 4");
     operation = readline.question();
   }
-
+  // Perform the operation on the two numbers.
   let output;
   let math;
   switch (operation) {
@@ -63,13 +65,15 @@ while (true) {
   );
 
   prompt(
-    `Do you want to do another calculation? Type 'y' for yes or 'n' for no.`
+    `Do you want to do another calculation? Type 'y' for yes or 'n' for no.  The program will end`
   );
 
   let answer = readline.question();
-
-  if (answer !== "y") break;
+  let end = prompt(`Have a nice day!`);
+  answer === "y" ? startCalculating() : end;
 }
+
+startCalculating();
 
 //Launch School version
 // const readline = require("readline-sync");
