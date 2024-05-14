38b3090338d16085ea4e8adb50b8a65abb21d35c
Generosa Litton <glcodeworks@gmail.com>
2024-05-13 19:27:23 -0500

    renamed files

--- calculator-orignal.js
+++ calculator-orignal.js
@@ -4,39 +4,21 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function invalidNumber(number) {
-  return number.trimStart() === "" || Number.isNaN(Number(number));
-}
-
 prompt("Welcome to Calculator!");
 // Ask the user for the first number.
 prompt("What's the first number? ");
 let number1 = readline.question();
 
-while (invalidNumber(number1)) {
-  prompt(`Sorry you entered an invalid first number. Try again.`);
-  number1 = readline.question();
-}
 // Ask the user for the second number.
 prompt("What's the second number? ");
 let number2 = readline.question();
-while (invalidNumber(number2)) {
-  prompt(`Sorry you entered an invalid second number. Try again.`);
-  number2 = readline.question();
-}
-
 prompt(`You entered these numbers: ${number1} and ${number2}`);
 
 // Ask the user for an operation to perform.
 prompt(
-  "What operation would you like to perform? 1) Add 2) Subtract 3) Multiply 4) Divide"
+  "What operation would you like to perform?\n 1) Add 2) Subtract 3) Multiply 4) Divide"
 );
 let operation = readline.question();
-
-while (!["1", "2", "3", "4"].includes(operation)) {
-  prompt("Must choose 1, 2, 3 or 4");
-  operation = readline.question();
-}
 // Perform the operation on the two numbers.
 let output;
 let math;
@@ -59,6 +41,20 @@ switch (operation) {
     break;
 }
 
+// if (operation === "1") {
+//   math = "adding";
+//   output = Number(number1) + Number(number2);
+// } else if (operation === "2") {
+//   math = "subtracting";
+//   output = Number(number1) - Number(number2);
+// } else if (operation === "3") {
+//   math = "multiplying";
+//   output = Number(number1) * Number(number2);
+// } else {
+//   math = "dividing";
+//   output = Number(number1) / Number(number2);
+// }
+// Print the result to the terminal.
 prompt(
   `The result of ${math} the numbers ${number1} and ${number2} is ${output} `
 );
